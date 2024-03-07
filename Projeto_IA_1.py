ccc800c0dd39d467322316f1c5781e1f8c34175d
Mando1mS <up202108859@g.uporto.pt>
2024-03-07 22:44:38 +0000

    Adicionada a leitura correta dos ficheiros

--- Projeto_IA_1.py
+++ Projeto_IA_1.py
@@ -1,10 +1,15 @@
 import numpy as np
+from sys import exit
 
 class Library():
     def __init__(self,n_livros,tempo_signup,livros_dia,livros):
+        # Numero de livros na livraria
         self.n_livros = n_livros
+        # Tempo que demora a registar
         self.tempo_signup = tempo_signup
+        # Quantos livros por dia consegue registar
         self.livros_dia = livros_dia
+        # Array com todos os IDs dos livros
         self.livros = livros
         
         
@@ -28,8 +33,25 @@ def ler_opcao(op):
 
 
 def main(op):
+    # Nome do ficheiro
     filename = ler_opcao(op)
-    print(filename + "\n")
+    # Abertura do ficheiro
+    file = open(filename,"r")
+    # Numero total de livros | Numero total de livrarias | Limite de dias 
+    nlivros,nlib,deadline=map(int,file.readline().split())
+    # Array com todas as pontuações dos livros, o livro numero 3 tem pontuação de socre[2]
+    scores=map(int,file.readline().split())
+    # Lista com todas as livrarias, organizadas pela class criada acima
+    lib = list()
+    for i in range(nlib):
+        nl,ts,ld=map(int,file.readline().split())
+        idlivros=map(int,file.readline().split())
+        lib.append(Library(nl,ts,ld,idlivros))
+    print(lib[0].n_livros)
+    # Fecha o ficheiro
+    file.close()
+    # Sai do programa (Por enquanto mete-se isto para nao voltar logo ao menu e ser mais facil ler o resultado)
+    exit()
     
     
 
